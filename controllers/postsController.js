d3c2297390d78c351aa7e4119400eac37621c6f9
Emmanuel Adiba <eabaagah@gmail.com>
2021-09-04 02:13:46 +0000

    posts controller

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,3 +1,15 @@
-export const getPosts = (req, res, next) => {
-    res.send("It Works")
+import PostMessage from "../models/postMessage.js"
+
+export const getPosts = async (req, res, next) => {
+    try {
+        const postMessages = await PostMessage.find()
+        
+        res.status(200).json(postMessages)
+    } catch (err) {
+        res.status(404).json({ message: err.message })
+    }
 }
+
+export const createPosts = (req, res, next) => {
+    res.send('Post Creation')
+} 
--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json({ limit: "30mb", extented: true }))
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}))
 app.use(cors())
 
-const CONNECTION_URL = 'mongodb+srv://aweperi:Trinity1234@cluster0.ctxgm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const CONNECTION_URL='mongodb+srv://aweperi:Trinity1234@cluster0.ctxgm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
