ba36a169622741b87db7e83ef6a01d24f05b6b77
Emmanuel Adiba <eabaagah@gmail.com>
2021-09-07 03:11:58 +0000

    configure .env file

--- index.js
+++ index.js
@@ -2,25 +2,22 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
-
+import dotenv from 'dotenv'
 import postRoutes from './routes/posts.js'
 
 const app= express()
 
-
+dotenv.config()
 
 app.use(express.json({ limit: "30mb", extented: true }))
 app.use(express.urlencoded({ limit: "30mb", extended: true}))
 app.use(cors())
 
 app.use('/posts', postRoutes)
-const live_api = true
-const cloudURI = 'mongodb+srv://aweperi:Trinity1234@cluster0.ctxgm.mongodb.net/memories?retryWrites=true&w=majority&ssl=true'
-const localDb = 'mongodb://localhost:27017/memories'
-const CONNECTION_URL= live_api ?  cloudURI : localDb
+
 const PORT = process.env.PORT || 5000
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => {
         app.listen(PORT, () => {
             console.log(`Server running on port: ${PORT}`);
