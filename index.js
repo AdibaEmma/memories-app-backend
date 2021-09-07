6d62246032b9e44f026170a50e8940d8c45fbe43
Emmanuel Adiba <eabaagah@gmail.com>
2021-09-07 00:37:48 +0000

    update

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors())
 
 app.use('/posts', postRoutes)
 
-const CONNECTION_URL='mongodb+srv://aweperi:Trinity1234@cluster0.ctxgm.mongodb.net/memories?retryWrites=true&w=majority'
+const CONNECTION_URL='mongodb+srv://aweperi:Trinity1234@cluster0.ctxgm.mongodb.net/memories?retryWrites=true&w=majority&ssl=true'
 const PORT = process.env.PORT || 5000
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
