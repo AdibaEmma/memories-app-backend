7c6cc146f97096f11bbe5bf446ef15b6774e3bfc
Emmanuel Adiba <eabaagah@gmail.com>
2022-03-06 10:59:28 +0000

    update

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -3,7 +3,6 @@ import PostMessage from "../models/postMessage.js"
 
 export const createPosts = async (req, res, next) => {
     const post = req.body
-    console.log(req.headers);
     const newPost = new PostMessage({...post, creator: req.userId, createdAt: new Date().toISOString()})
 
     try {
