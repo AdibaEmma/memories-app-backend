a6efb5cd174c685247681df40b8eae0ddd4cc0ce
Emmanuel Adiba <eabaagah@gmail.com>
2022-04-22 17:22:15 +0000

    update like post

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -72,13 +72,14 @@ export const likePost = async (req, res, next) => {
     try {
         const { id } = req.params
 
+
         if(!req.userId) return res.status(404).json({ message: 'Unauthenticated' })
 
         if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No post with that id')
 
         const post = await PostMessage.findById(id)
 
-        const index = post.likes.findIndex(id => id === String(req.userId))
+        const index = post.likes.findIndex(likeId => likeId === String(req.userId))
 
         if(index === -1) {
             post.likes.push(req.userId)
@@ -86,7 +87,7 @@ export const likePost = async (req, res, next) => {
             post.likes = post.likes.filter(id => id !== String(req.userId))
         }
 
-        const updatedPost = await PostMessage.findByIdAndUpdate(id, { post}, { new: true })
+        const updatedPost = await PostMessage.findByIdAndUpdate(id, { post }, { new: true })
 
         res.status(200).json(updatedPost)
     } catch (err) {
